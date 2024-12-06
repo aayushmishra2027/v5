34d950eb129267e6ad6d136aa854dbbf1326b065
Aayush Mishra <aayushmishra1105@gmail.com>
2024-12-07 02:10:55 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <section id="home" className="home">
+            <h1>Welcome to My Portfolio</h1>
+            <p>I'm a passionate developer, designer, and creator.</p>
+            <a href="#about" className="btn">Learn More</a>
+        </section>
+    );
+};
+
+export default Home;
