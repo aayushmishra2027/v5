3eca89a0943846e5b9d52dd504e42af3e7ede0b3
Aayush Mishra <aayushmishra1105@gmail.com>
2024-12-07 02:12:14 +0530

    Create Skills.js

---
+++ Skills.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Skills.css';
+
+const Skills = () => {
+    return (
+        <section id="skills" className="skills">
+            <h2>My Skills</h2>
+            <ul className="skills-list">
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Three.js</li>
+                <li>CSS & HTML</li>
+                <li>UI/UX Design</li>
+            </ul>
+        </section>
+    );
+};
+
+export default Skills;
