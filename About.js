0f6103794a55c0d0eec120374a562c9aa919c55a
Aayush Mishra <aayushmishra1105@gmail.com>
2024-12-07 02:11:45 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <section id="about" className="about">
+            <h2>About Me</h2>
+            <p>
+                I am a dedicated professional with a passion for technology and design. 
+                With experience in web development, I strive to create beautiful and functional applications.
+            </p>
+            <p>
+                In my free time, I enjoy exploring new technologies, contributing to open-source projects, 
+                and sharing my knowledge through blogging and tutorials.
+            </p>
+        </section>
+    );
+};
+
+export default About;
